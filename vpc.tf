e28ba6d30ede5c68277caee7667ebf6ee5b25a9c
root <Hannes Reinhardt DB.com>
2023-05-12 15:35:05 +0000

    14. try to vpc module

--- vpc.tf
+++ vpc.tf
@@ -3,13 +3,51 @@ module "vpc" {
     version = "~> 3.4"
 
     project_id = var.project
-    network_name = "gaurav-network"
+    network_name = "khr-network"
+    routing_mode = "GLOBAL"
 
    subnets = [
   {
-    subnet_name   = "gaurav-subnet"
+    subnet_name   = "khr-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
-  }
-] 
+          },
+        {
+            subnet_name           = "khr-subnet-02"
+            subnet_ip             = "10.10.20.0/24"
+            subnet_region         = var.region
+            subnet_private_access = "true"
+            subnet_flow_logs      = "true"
+            description           = "This subnet has a description"
+        },
+        {
+            subnet_name               = "khr-subnet-03"
+            subnet_ip                 = "10.10.30.0/24"
+            subnet_region             = var.region
+            subnet_flow_logs          = "true"
+            subnet_flow_logs_interval = "INTERVAL_10_MIN"
+            subnet_flow_logs_sampling = 0.7
+            subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
+        }
+    ]
+
+    
+
+    routes = [
+        {
+            name                   = "egress-internet"
+            description            = "route through IGW to access internet"
+            destination_range      = "0.0.0.0/0"
+            tags                   = "egress-inet"
+            next_hop_internet      = "true"
+        },
+        {
+            name                   = "khr-app-proxy"
+            description            = "route through proxy to reach app"
+            destination_range      = "10.50.10.0/24"
+            tags                   = "khr-app-proxy"
+            next_hop_instance      = "khr-app-proxy-instance"
+            next_hop_instance_zone = var.zone
+        },
+    ]
 }
