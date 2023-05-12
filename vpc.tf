7722367a768457dfb89ebb4702801a470abd2540
root <Hannes Reinhardt DB.com>
2023-05-12 14:17:03 +0000

    3rd try to vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     source  = "terraform-google-modules/network/google"
-    version = "~> 7.0"
+    version = "~> 3.4"
 
     project_id   = "<PROJECT ID>"
     network_name = "example-vpc"
